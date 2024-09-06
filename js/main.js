13852489a7f784b589e72790403cb16245dc7c8e
Vitor Emanuel <133882924+vitinn12@users.noreply.github.com>
2024-09-06 01:06:01 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+window.revelar = ScrollReveal ({reset: true})
+
+revelar.reveal('.imagem_comp1, .imagem',{
+    duration:1000,
+    distance: '100px'
+});
+
+revelar.reveal('.texto_comp1,.texto_comp2',{
+    duration:1000,
+    distance: '100px',
+    delay: 500
+}); 
