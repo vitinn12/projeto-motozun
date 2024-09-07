242bae2f867caf102baf04cf0e7daf412e432c0b
vitinn12 <vitoralves1212.va@gmail.com>
2024-09-06 22:41:37 -0300

    3.0

--- js/main.js
+++ js/main.js
@@ -5,8 +5,18 @@ revelar.reveal('.imagem_comp1, .imagem',{
     distance: '100px'
 });
 
-revelar.reveal('.texto_comp1,.texto_comp2, .card=comp2',{
+revelar.reveal('.texto_comp1,.texto_comp2, .card-comp2',{
     duration:1000,
     distance: '100px',
     delay: 500
 }); 
+revelar.reveal('.card-contato',{
+    duration:1000,
+    origin:'right',
+    distance:'100px',
+    delay: 500,
+    reset:false
+
+});
+
+
