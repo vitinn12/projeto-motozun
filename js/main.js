83c90d852f93a1fc811e12db2123702ad7cf11e5
vitinn12 <vitoralves1212.va@gmail.com>
2024-09-09 07:57:33 -0300

    Finalizado

--- js/main.js
+++ js/main.js
@@ -2,13 +2,15 @@ window.revelar = ScrollReveal ({reset: true})
 
 revelar.reveal('.imagem_comp1, .imagem',{
     duration:1000,
-    distance: '100px'
+    distance: '100px',
+    reset: false
 });
 
 revelar.reveal('.texto_comp1,.texto_comp2, .card-comp2',{
     duration:1000,
     distance: '100px',
-    delay: 500
+    delay: 500,
+    reset: false
 }); 
 revelar.reveal('.card-contato',{
     duration:1000,
